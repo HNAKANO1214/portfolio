1f703b6c90325fdd5f0fbb0afa0b3372f8d92b33
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-05 12:14:50 +0900

    renderの休眠防止スクリプト登録

---
+++ myapp/commands/request_render.py
@@ -0,0 +1,20 @@
+import requests
+import schedule
+import time
+
+
+def send_request():
+    url = "https://portfolio-web-gjle.onrender.com/"
+    try:
+        response = requests.get(url)
+        print(f"Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# 10分ごとに実行するようにスケジュール設定
+schedule.every(10).minutes.do(send_request)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
