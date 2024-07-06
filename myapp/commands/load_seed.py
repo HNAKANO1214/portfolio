4a0bfef79915d9e079fe91ec9a90d8220898062e
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-07 01:52:42 +0900

    ルートディレクトリの指定を変更

--- myapp/commands/load_seed.py
+++ myapp/commands/load_seed.py
@@ -4,7 +4,7 @@ import sys
 import django
 from django.core.management import call_command
 
-# sys.path.append('/app')
+sys.path.append('/home/runner/work/portfolio/portfolio')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
