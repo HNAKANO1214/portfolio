85b4e8f216523149dbc9419b7a63cbcc8114c0f1
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-07 00:20:06 +0900

    シードデータ及びロードコマンド作成

---
+++ myapp/commands/load_seed.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+import django
+from django.core.management import call_command
+
+sys.path.append('/app')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
+LOAD_SEED_DATA = [
+    'myapp/fixtures/seeders/profile.json',
+    'myapp/fixtures/seeders/work.json',
+    'myapp/fixtures/seeders/education.json',
+    'myapp/fixtures/seeders/experience.json',
+    'myapp/fixtures/seeders/software.json',
+    'myapp/fixtures/seeders/technical.json',
+]
+
+
+def load_seed_data():
+    print('Loading seed data...')
+    for seed_data in LOAD_SEED_DATA:
+        print(f'Loading {seed_data}...')
+        call_command('loaddata', seed_data)
+        print(f'{seed_data} loaded.')
+    print('Seed data loaded.')
+
+
+if __name__ == '__main__':
+    load_seed_data()
--- myapp/tests/conftest.py
+++ myapp/tests/conftest.py
@@ -1,11 +1,9 @@
 import pytest
-from django.core.management import call_command
+
+from myapp.commands.load_seed import load_seed_data
 
 
 @pytest.fixture(scope='session', autouse=True)
-def load_seed_data(django_db_setup, django_db_blocker):
+def test_load_seed_data(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        # シードデータをロードする
-        print('Loading seed data...')
-        call_command('loaddata', 'myapp/fixtures/seeders/work.json')
-        print('Seed data loaded.')
+        load_seed_data()
