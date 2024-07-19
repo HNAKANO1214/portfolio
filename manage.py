36f60e6e0fa49890792a5dea18803221b4321512
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-20 01:44:56 +0900

    デバッグ用コミット
    print文出力

--- manage.py
+++ manage.py
@@ -6,9 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print(os.environ.get('DJANGO_SETTINGS_MODULE'))
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'mysite.settings.local')
     )
+    print(os.environ.get('DJANGO_SETTINGS_MODULE'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
