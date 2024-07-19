52940fb908876c70edc4dfe4be580a60ab2d38a8
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-20 03:53:29 +0900

    静的ファイル収集のための設定を修正

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,7 @@
 from dj_database_url import parse
 
 from .base import *     # noqa: F401, F403
-from .base import env, MIDDLEWARE
+from .base import BASE_DIR, env, MIDDLEWARE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -16,4 +16,10 @@ DATABASES = {
 }
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
