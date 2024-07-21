58893c1617c3c372ac2bea5cfa923ed068d55b8c
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-21 22:56:52 +0900

    Favicon、Logoの呼び込み方法を変更

--- myapp/context_processors.py
+++ myapp/context_processors.py
@@ -1,10 +1,7 @@
-import os
-
 from django.conf import settings
 
 
 def environment_variables(request):
     return {
         'SITE_TITLE': settings.SITE_TITLE,    # サイトのタイトル
-        'MEDIA_URL': settings.MEDIA_URL,      # メディアファイルのURL
     }
