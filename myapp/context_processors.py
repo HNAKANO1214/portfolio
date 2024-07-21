a500c3122499786442d29640dc02f46d921733be
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-21 22:22:04 +0900

    Favicon、LOGO画像を追加

--- myapp/context_processors.py
+++ myapp/context_processors.py
@@ -6,4 +6,5 @@ from django.conf import settings
 def environment_variables(request):
     return {
         'SITE_TITLE': settings.SITE_TITLE,    # サイトのタイトル
+        'MEDIA_URL': settings.MEDIA_URL,      # メディアファイルのURL
     }
