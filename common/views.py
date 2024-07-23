7aa140d1d99d2f253a3bdfc48f0c932411d017ca
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-24 06:25:44 +0900

    キャッシュクリアのタイミング変更

--- common/views.py
+++ common/views.py
@@ -5,8 +5,8 @@ from django.utils import translation
 
 
 def set_language(request, lang_code):
-    translation.activate(lang_code)
     cache.clear()   # キャッシュクリア
+    translation.activate(lang_code)
     response = HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
     return response
