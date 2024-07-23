3ed5c4543f0f64140697dd8e1b8965b77f188ed1
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-07-24 06:56:16 +0900

    キャッシュクリア処理を削除

--- common/views.py
+++ common/views.py
@@ -1,11 +1,9 @@
 from django.conf import settings
-from django.core.cache import cache
 from django.http import HttpResponseRedirect
 from django.utils import translation
 
 
 def set_language(request, lang_code):
-    cache.clear()   # キャッシュクリア
     translation.activate(lang_code)
     response = HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
